9a848f6338914363a2df49fb898caddc9a17af52
masudmolla6 <masudmolla2937.com>
2026-02-05 20:02:12 +0600

    buit_in_function

--- Module 13/ceil.c
+++ Module 13/ceil.c
@@ -1,7 +1,12 @@
 #include <stdio.h>
 #include <math.h>
 int main() {
-    int result=ceil(3.2345);
+    // int result=ceil(3.2345);
+    // int result=floor(3.2345);
+    // int result=round(3.2345);
+    // int result=sqrt(36);
+    // int result=pow(3,2);
+    int result=abs(3,2);
     printf("%d", result);
     return 0;
 }
