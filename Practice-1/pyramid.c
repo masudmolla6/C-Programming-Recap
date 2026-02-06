93a7deb8b6aea59d7db9f4010163221be3f79760
masudmolla6 <masudmolla2937@gmail.com>
2026-02-06 18:42:31 +0600

    Pratice Criticle Logic

---
+++ Practice-1/pyramid.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+int main() {
+    int n, star=1;
+    scanf("%d", &n);
+    int space=n-1;
+    for (int i = 1; i <= n; i++)
+    {
+        for (int j = 1; j <= space; j++)
+        {
+            printf(" ");
+        }
+        for (int k = 1; k <= star; k++)
+        {
+            printf("*");
+        }
+        
+        printf("\n");
+        star+=2;
+        space--;
+    }
+    
+    return 0;
+}
