96b8317d9cb78c546b113021c0119585f30c5e10
masudmolla6 <masudmolla2937@gmail.com>
2026-02-14 23:08:53 +0600

    digit with recursion

---
+++ Module 19/D_Print_Digits_using_Recursion.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+void rec(int n){
+    if (n==0)
+    {
+        return;
+    }
+    rec(n/10);
+    int last=n%10;
+    printf("%d ", last);
+}
+int main() {
+    int ts;
+    scanf("%d", &ts);
+    while (ts--)
+    {
+        int n;
+        scanf("%d", &n);
+        if (n==0)
+        {
+            printf("0");
+        }
+        rec(n);
+        printf("\n");
+    }
+    return 0;
+}
