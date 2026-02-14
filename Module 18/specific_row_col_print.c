ac3054044227e0984fbcb502fba1106a8de842c7
masudmolla6 <masudmolla2937@gmail.com>
2026-02-14 12:12:34 +0600

    2d array

---
+++ Module 18/specific_row_col_print.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+int main() {
+    int r,c;
+    scanf("%d %d", &r,&c);
+    int a[r][c];
+    for (int i = 0; i < r; i++)
+    {
+        for (int j = 0; j < c; j++)
+        {
+            scanf("%d", &a[i][j]);
+        }
+    }
+    
+    // int sp_row;
+    // scanf("%d", &sp_row);
+    // for (int i = 0; i < c; i++)
+    // {
+    //     printf("%d ", a[sp_row][i]);
+    // }
+
+    int sp_col;
+    scanf("%d", &sp_col);
+    for (int i = 0; i < r; i++)
+    {
+        printf("%d\n", a[i][sp_col]);
+    }
+    
+    return 0;
+}
